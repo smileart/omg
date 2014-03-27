065e9c6edcd1566d24c8e252c075d57f96fc2e4b
Smile @rT <smileart21@gmail.com>
2014-03-27 15:37:50 +0200

    Minor bug fixes and improvements OS X aliases in .smileart.aliases moved under $OSTYPE if Pair wemux users administration aliases semantically renamed Installation and build scripts have been renamed with no .sh ext Fixed broken Debian cmake for tmux requirement Fixed Debian wemux installation with sudo

--- packages/debian/tmux/tmux.sh
+++ packages/debian/tmux/tmux.sh
@@ -1,4 +1,4 @@
-pkg_prereqs=('apt-get cmake git')
+pkg_prereqs=('apt-get git')
 pkg_extract_path=~/
 pkg_description='tmux — best terminal multiplyer'
 
@@ -11,8 +11,7 @@ function install_package() {
   cmake .
   make
   sudo make install
-  cd /tmp
-  git clone git://github.com/zolrath/wemux.git /usr/local/share/wemux
+  sudo git clone git://github.com/zolrath/wemux.git /usr/local/share/wemux
   cd wemux/
   sudo ln -s /usr/local/share/wemux/wemux /usr/local/bin/wemux
   sudo cp /usr/local/share/wemux/wemux.conf.example /usr/local/etc/wemux.conf
