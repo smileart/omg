c08d1d218795a37f318a363455fe72181ef92a89
Smile @rT <smileart21@gmail.com>
2014-08-03 17:41:47 +0300

    Added ruby package for debian-like systems Deleted rails package from antigen plugins list Improved Ruby installation for OS X system + version updated to 2.1.2 Ubuntu image version updated up to 14.04

--- packages/debian/ruby/ruby.sh
+++ packages/debian/ruby/ruby.sh
@@ -1,9 +1,22 @@
-pkg_prereqs=('brew grep')
+pkg_prereqs=('git grep')
 pkg_extract_path=~/
 pkg_description="ruby — A PROGRAMMER'S BEST FRIEND"
 
 function install_package() {
-  brew install rbenv ruby-build rbenv-gem-rehash rbenv-gemset
+
+  cd
+  rm -rf ~/.rbenv
+
+  git clone git://github.com/sstephenson/rbenv.git .rbenv
+  source ~/.bashrc
+  source ~/.omgzsh
+
+  git clone git://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
+  echo 'export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"' >> ~/.bashrc
+  echo 'export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"' >> ~/.omgzsh
+
+  source ~/.bashrc
+  source ~/.omgzsh
 
   rbenv install 2.1.2
   rbenv global 2.1.2
