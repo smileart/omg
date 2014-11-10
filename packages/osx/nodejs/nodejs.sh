a52c5156ef92a1a7185af21ac369b6ab2aae5acf
Smile @rT <smileart21@gmail.com>
2014-11-10 19:24:16 +0200

    Fixed node.js install on OS X

--- packages/osx/nodejs/nodejs.sh
+++ packages/osx/nodejs/nodejs.sh
@@ -3,11 +3,13 @@ pkg_extract_path=~/
 pkg_description='node.js — server-side JS platform'
 
 function install_package() {
+    brew cask zap node
     brew cask fetch node
     brew cask install node
     npm update npm -g
     npm install -g n
     n latest
+    sudo chown -R $USER /usr/local/lib/node_modules
     brew cleanup --force
     brew cask cleanup
     rm -f -r /Library/Caches/Homebrew/*
