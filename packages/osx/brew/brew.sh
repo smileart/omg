58b23093f1f610fa3a61136caa6cf9f0007ceb0f
Smile @rT <smileart21@gmail.com>
2015-10-30 17:17:32 +0200

    Fix brew & brew-cask installation package

--- packages/osx/brew/brew.sh
+++ packages/osx/brew/brew.sh
@@ -3,7 +3,7 @@ pkg_extract_path=~/
 pkg_description='brew - the missing package manager for OS X'
 
 function install_package() {
-  ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
-  brew tap phinze/homebrew-cask
-  brew install brew-cask
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  brew tap phinze/cask
+  brew install phinze/cask/brew-cask
 }
