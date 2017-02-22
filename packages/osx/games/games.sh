117a554d1689142e58fd0ccaed662b1633bbe72d
Smile @rT <smileart21@gmail.com>
2017-02-22 16:11:03 +0530

    Add a new `games` package for MacOS

---
+++ packages/osx/games/games.sh
@@ -0,0 +1,10 @@
+pkg_prereqs=('brew')
+pkg_extract_path=~/
+pkg_description='games — All work and no play makes Jack a dull boy'
+
+function install_package() {
+  brew install nethack
+  brew install ninvaders
+  brew install vitetris
+  brew install typespeed
+}
