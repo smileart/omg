9b97ab13d55c62d5dfacccf54f160eb8018c285d
Smile @rT <smileart21@gmail.com>
2017-02-23 19:18:16 +0200

    Add a few games to the `games` package for MacOS

--- packages/osx/games/games.sh
+++ packages/osx/games/games.sh
@@ -5,13 +5,16 @@ pkg_description='games — All work and no play makes Jack a dull boy'
 function install_package() {
   brew install nethack
   brew install ninvaders
-  brew install vitetris
   brew install typespeed
   brew install tintin
   brew install greed
   brew install nudoku
   brew install moon-buggy
+  brew install nsnake
 
   brew install pacman4console
   brew install myman
+
+  brew install vitetris
+  brew install bastet
 }
