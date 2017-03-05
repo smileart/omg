0879d56b3c0a6e6e33e8608ea92e2ba1a6060913
Smile @rT <smileart21@gmail.com>
2017-03-05 10:04:19 +0530

    Fix `color` module shellcheck concerns

--- modules/color.sh
+++ modules/color.sh
@@ -1,23 +1,24 @@
-module_color=1
+#!/usr/bin/env bash
+export module_color=1
 
 # Color constants
-ansi_black='\033[30m'
-ansi_black_bold='\033[0;30;1m'
-ansi_red='\033[31m'
-ansi_red_bold='\033[0;31;1m'
-ansi_green='\033[32m'
-ansi_green_bold='\033[0;32;1m'
-ansi_yellow='\033[33m'
-ansi_yellow_bold='\033[0;33;1m'
-ansi_blue='\033[34m'
-ansi_blue_bold='\033[0;34;1m'
-ansi_magenta='\033[35m'
-ansi_magenta_bold='\033[0;35;1m'
-ansi_cyan='\033[36m'
-ansi_cyan_bold='\033[0;36;1m'
-ansi_white='\033[37m'
-ansi_white_bold='\033[0;37;1m'
-ansi_reset='\033[0m'
+export ansi_black='\033[30m'
+export ansi_black_bold='\033[0;30;1m'
+export ansi_red='\033[31m'
+export ansi_red_bold='\033[0;31;1m'
+export ansi_green='\033[32m'
+export ansi_green_bold='\033[0;32;1m'
+export ansi_yellow='\033[33m'
+export ansi_yellow_bold='\033[0;33;1m'
+export ansi_blue='\033[34m'
+export ansi_blue_bold='\033[0;34;1m'
+export ansi_magenta='\033[35m'
+export ansi_magenta_bold='\033[0;35;1m'
+export ansi_cyan='\033[36m'
+export ansi_cyan_bold='\033[0;36;1m'
+export ansi_white='\033[37m'
+export ansi_white_bold='\033[0;37;1m'
+export ansi_reset='\033[0m'
 
 # Reset text attributes to normal without clearing screen.
 alias Reset="tput sgr0"
