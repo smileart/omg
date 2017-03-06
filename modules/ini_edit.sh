312e38f14e9eb6b6cdaabdb8f3e7fcbbf501ec80
Smile @rT <smileart21@gmail.com>
2017-03-06 17:48:01 +0530

    Fix `ini_edit` module shellcheck concerns

--- modules/ini_edit.sh
+++ modules/ini_edit.sh
@@ -1,5 +1,7 @@
+#!/usr/bin/env bash
+
 # Source: https://gist.github.com/smileart/4597fa6a8afdf659d1d6
-module_ini_edit=1
+export module_ini_edit=1
 
 # param_1: string | param_2: file
 function b.is_option_in_file () {
@@ -18,7 +20,7 @@ function b.is_option_in_file () {
 # param_1: string | param_2: file
 function b.get_option_line_number () {
   if [[ ! -z "$1" && ! -z "$2" ]]; then
-   echo $(egrep -n "$1" "$2" | cut -f1 -d:)
+   echo "$(egrep -n "$1" "$2" | cut -f1 -d:)"
   fi
 }
 
@@ -29,8 +31,8 @@ function b.ini_set_option () {
   local original_option="$1"
   local new_option_value="$2"
 
-  if [ $( b.is_option_in_file "$ini_option" "$ini_file" ) = "1" ]; then
-    echo "Changed $1 value to $2 in the lines: " $(b.get_option_line_number "$ini_option" "$ini_file")
+  if [ "$( b.is_option_in_file "$ini_option" "$ini_file" )" = "1" ]; then
+    echo "Changed $1 value to $2 in the lines: " "$(b.get_option_line_number "$ini_option" "$ini_file")"
     sed -i -e "s/$ini_option/$original_option=$new_option_value/g" $ini_file
   else
     echo "$original_option=$new_option_value " >> $ini_file
@@ -42,8 +44,8 @@ function b.ini_comment_option () {
   local ini_file="$2";
   local ini_option="^$1.*$";
 
-  if [ $( is_option_in_file "$ini_option" "$ini_file" ) = "1" ]; then
-    echo "Commented $1 option in the lines: " $(b.get_option_line_number "$ini_option" "$ini_file")
+  if [ "$( is_option_in_file "$ini_option" "$ini_file" )" = "1" ]; then
+    echo "Commented $1 option in the lines: " "$(b.get_option_line_number "$ini_option" "$ini_file")"
     sed -i -e "s/\($ini_option\)/;\1/g" $ini_file
   fi
 }
@@ -53,8 +55,8 @@ function b.ini_uncomment_option () {
   local ini_file="$2";
   local ini_option="^;$1.*$";
 
-  if [ $( is_option_in_file "$ini_option" "$ini_file" ) = "1" ]; then
-    echo "Uncommented $1 option in the lines: " $(b.get_option_line_number "$ini_option" "$ini_file")
+  if [ "$( is_option_in_file "$ini_option" "$ini_file" )" = "1" ]; then
+    echo "Uncommented $1 option in the lines: " "$(b.get_option_line_number "$ini_option" "$ini_file")"
     sed -i -e "/$ini_option/ s/^;//" $ini_file
   fi
 }
