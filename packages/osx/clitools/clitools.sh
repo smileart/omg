7042fe344629666f47e002eced0d194fd4f2cec6
Smile @rT <smileart21@gmail.com>
2017-03-18 18:18:12 +0530

    Change `clitools` installation for MacOS to GUI-less approach

--- packages/osx/clitools/clitools.sh
+++ packages/osx/clitools/clitools.sh
@@ -2,14 +2,28 @@ pkg_prereqs=('xcode-select')
 pkg_extract_path=~/
 pkg_description='XCode CLI Tools - large suite of software development tools and libraries from Apple'
 
+# See http://apple.stackexchange.com/questions/107307/how-can-i-install-the-command-line-tools-completely-from-the-command-line
+# See http://www.mokacoding.com/blog/how-to-install-xcode-cli-tools-without-gui/
 function install_package() {
-    # OS X Command Line Tools
+  # OS X Command Line Tools
   if [[ ! -z "/Library/Developer/CommandLineTools/" ]]; then
-    xcode-select --install
-    b.color.cecho $ansi_red "First: Choose install XCode on dialog window!"
-    open -a XCode
-    b.color.cecho $ansi_red "Second: Click Agree on License dialog window!"
-    b.color.cecho $ansi_red "Then: Run clitools installation again and click Install"
+    b.color.cecho $ansi_yellow "Checking Xcode CLI tools"
+    # Only run if the tools are not installed yet
+    # To check that try to print the SDK path
+    xcode-select -p &> /dev/null
+
+    if [ $? -ne 0 ]; then
+      b.color.cecho $ansi_yellow "Xcode CLI tools not found. Installing them..."
+      touch /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress;
+      PROD=$(softwareupdate -l |
+      grep "\*.*Command Line" |
+      head -n 1 | awk -F"*" '{print $2}' |
+      sed -e 's/^ *//' |
+      tr -d '\n')
+      softwareupdate -i "$PROD" -v;
+    else
+      b.color.cecho $ansi_green "Xcode CLI tools are already installed!"
+    fi
   else
     sudo xcode-select --switch /Library/Developer/CommandLineTools/
   fi
