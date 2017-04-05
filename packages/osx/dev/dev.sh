7bb41ef431fdd099bd8be4422d2f10ff8f036f37
Smile @rT <smileart21@gmail.com>
2017-04-05 23:34:17 +0530

    Add Redis to the `dev` package for MacOS Add PostgreSQL to the `dev` package for MacOS Add MongoDB to the `dev` package for MacOS

--- packages/osx/dev/dev.sh
+++ packages/osx/dev/dev.sh
@@ -5,4 +5,7 @@ pkg_extract_path=~/
 pkg_description='Dev tools/DBs/apps'
 
 function install_package() {
+  b.system.optional_brew_install_new "Redis" redis
+  b.system.optional_brew_install_new "PostgreSQL" postgres
+  b.system.optional_brew_install_new "MongoDB" mongodb
 }
