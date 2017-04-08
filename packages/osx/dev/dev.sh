e4563ed90b05bddca927e780c590c41578eb025b
Smile @rT <smileart21@gmail.com>
2017-04-08 23:08:21 +0530

    Add admin apps for the DBs in the dev package for MacOS

--- packages/osx/dev/dev.sh
+++ packages/osx/dev/dev.sh
@@ -1,11 +1,18 @@
 #!/usr/bin/env bash
 
-pkg_prereqs=('brew')
+pkg_prereqs=('brew' 'node')
 pkg_extract_path=~/
 pkg_description='Dev tools/DBs/apps'
 
 function install_package() {
   b.system.optional_brew_install_new "Redis" redis
+  if b.ui.ask_yes_or_not ">>> Would you like to install Redis Commander app?"; then
+    npm install -g redis-commander
+  fi
+
   b.system.optional_brew_install_new "PostgreSQL" postgres
+  b.system.optional_brew_cask_install_new "pgAdmin 4" pgadmin4
+
   b.system.optional_brew_install_new "MongoDB" mongodb
+  b.system.optional_brew_cask_install_new "Robomongo" robomongo
 }
