81280639709f884cb5d38956223ae9dfc4db3fc8
Smile @rT <smileart21@gmail.com>
2017-04-15 22:58:01 +0530

    Add `usql` to dev package for MacOS Update README accordingly

--- packages/osx/dev/dev.sh
+++ packages/osx/dev/dev.sh
@@ -15,4 +15,16 @@ function install_package() {
 
   b.system.optional_brew_install_new "MongoDB" mongodb
   b.system.optional_brew_cask_install_new "Robomongo" robomongo
-}
+
+  if b.ui.ask_yes_or_not ">>> Would you like to install usql"; then
+    source $MY_PATH/files/go/.omg_aliases/.paths.env
+    echo "Go tools path: $GOTOOLS"
+
+    switch_to_go_tools
+    go get -u github.com/akrennmair/goauth > /dev/null 2>&1
+    cd $GOTOOLS/src/github.com/akrennmair/goauth
+    { git checkout master && git pull && cd -; } > /dev/null 2>&1
+    go get -u -tags most github.com/knq/usql
+    switch_to_go_path
+  fi
+ }
