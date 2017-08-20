751b1c47e3b888d751d0eaa9da14f71084c16fbc
Serge Bedzhyk <smileart21@gmail.com>
2017-08-20 13:14:35 +0300

    fix: update Docker install for MacOS to the modern version

--- packages/osx/docker/docker.sh
+++ packages/osx/docker/docker.sh
@@ -4,26 +4,6 @@ pkg_extract_path=~/
 pkg_description='docker - lightweight linux env isolation containers'
 
 function install_package() {
-  b.system.brew_cask_install_new virtualbox
-
-  brew update
-  brew upgrade
-
-  b.system.brew_install_new docker
-  b.system.brew_install_new docker-machine
-  b.system.brew_install_new docker-compose
-
-  docker-machine rm lab
-  docker-machine create --driver virtualbox --virtualbox-memory "4096" lab
-  docker-machine upgrade lab
-
-  touch ~/.user.aliases
-
-  if [[ $( b.framework.is_string_existed 'eval "$(docker-machine env lab)"' ~/.user.aliases ) == "0" ]]
-  then
-    echo 'eval "$(docker-machine env lab)"' >> ~/.user.aliases
-  fi
-
-  ruby <(curl -L https://git.io/vvvco) lab
-  source ~/.user.aliases
+  b.system.brew_cask_install_new docker
+  b.system.brew_install_new install docker-completion
 }
